f62d77557f6fa73ba6cd1166942a886b8c6b82c7
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2023-05-07 20:07:00 +0200

    create forecast

--- weather-app/src/Components/Weather/Weather.js
+++ weather-app/src/Components/Weather/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Weather.css";
 
 const API_KEY = "446fe391c3d9431aa5b125418230705";
-const API_URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=`;
+const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=`;
 
 function Weather() {
   const [city, setCity] = useState("");
@@ -10,7 +10,7 @@ function Weather() {
   const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   async function fetchWeatherData() {
-    const response = await fetch(`${API_URL}${city}`);
+    const response = await fetch(`${API_URL}${city}&days=4`);
     const json = await response.json();
     setWeatherData(json);
   }
@@ -43,6 +43,19 @@ function Weather() {
           <h2>Current weather in {weatherData.location.name}</h2>
           <p>Temperature: {weatherData.current.temp_c}°C</p>
           <p>Condition: {weatherData.current.condition.text}</p>
+        </div>
+      )}
+      {isButtonClicked && weatherData && weatherData.forecast && (
+        <div className="forecast">
+          <h2>3-day Weather forecast for {weatherData.location.name}</h2>
+          {weatherData.forecast.forecastday.map((day) => (
+            <div key={day.date}>
+              <h3>{day.date}</h3>
+              <p>Max temperature: {day.day.maxtemp_c}°C</p>
+              <p>Min temperature: {day.day.mintemp_c}°C</p>
+              <p>Condition: {day.day.condition.text}</p>
+            </div>
+          ))}
         </div>
       )}
     </div>
